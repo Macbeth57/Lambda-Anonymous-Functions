50e8f851fb52ee88a92188c7a08949d188fe3915
Louis <macbeth574@gmail.com>
2024-03-11 11:03:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace NumPy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 15, 45, 25, 36, 17, 20, 0, 86};
+            Func<int,int> addTwo = x => x + 2;
+
+            list = MyFunction(list, addTwo);
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            list = MyFunction(list, x => x - 10);
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+        public static List<int> MyFunction(List<int> list, Func<int,int> anonFunc)
+        {
+            List<int> listToReturn = new List<int>();
+
+            foreach (int value in list)
+            {
+                listToReturn.Add(anonFunc(value));
+            }
+            return listToReturn;
+        }
+    }
+}
